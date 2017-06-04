48393ae435d15b0ca141accaffd7a324638ce4b3
AgtLucas <lucas@agtlucas.com>
2017-06-04 22:05:29 +0200

    Update filter

--- src/Filter.js
+++ src/Filter.js
@@ -1,15 +1,12 @@
-import React, { createElement } from 'react'
+import React from 'react'
 import { element, string, object, node, oneOfType } from 'prop-types'
 import mapFilters from 'map-css-filters'
 
-const Filter = ({ component = 'div', filter, children, ...props }) => {
-  const c = createElement(component)
-  return (
-    <c.type {...props} style={{filter: mapFilters(filter)}}>
-      {children}
-    </c.type>
-  )
-}
+const Filter = ({ filter, children, ...props }) => (
+  <div {...props} style={{filter: mapFilters(filter)}}>
+    {children}
+  </div>
+)
 
 Filter.propTypes = {
   component: oneOfType([element, string]),
