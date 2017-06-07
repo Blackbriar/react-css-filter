1fe982306c5774c5a98fba717df9be89be28e8ab
AgtLucas <lucas@agtlucas.com>
2017-06-07 08:04:15 +0200

    Format with prettier-standard :metal:

--- src/Filter.js
+++ src/Filter.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { element, string, object, node, oneOfType } from 'prop-types'
 import mapFilters from 'map-css-filters'
 
-const Filter = ({ effects, children, ...props }) => (
-  <div {...props} style={{filter: mapFilters(effects)}}>
+const Filter = ({ effects, children, ...props }) =>
+  <div {...props} style={{ filter: mapFilters(effects) }}>
     {children}
   </div>
-)
 
 Filter.propTypes = {
   component: oneOfType([element, string]),
